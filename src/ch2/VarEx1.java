9b638cd0a1ca11473b1f5d628e99c000cd90ede7
Joo <iotaxixi.gmail.com>
2022-05-04 01:49:30 +0900

    Chapter 02 변수 variable

--- src/ch2/VarEx1.java
+++ src/ch2/VarEx1.java
@@ -6,5 +6,11 @@ public class VarEx1 {
         int age = 14;
         System.out.println(year);
         System.out.println(age);
+
+        year = age + 2000;
+        age = age + 1;
+        System.out.println(year);
+        System.out.println(age);
+
     }
 }
