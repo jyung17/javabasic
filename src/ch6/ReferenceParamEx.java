6fc03f9d7ef1f6d052567d5a1c751f015b1529c1
iotaxixi <iotaxixi@gmail.com>
2022-05-16 21:55:00 +0900

    Chapter 06 객체지향

---
+++ src/ch6/ReferenceParamEx.java
@@ -0,0 +1,22 @@
+package ch6;
+class Data2 {
+    int x;
+}
+
+public class ReferenceParamEx {
+    public static void main(String[] args) {
+        Data2 d = new Data2();
+        d.x = 10;
+        System.out.println("main() : x =  " + d.x);
+
+        change2(d);
+        System.out.println("After change(d)");
+        System.out.println("main() : x = " + d.x);
+    }
+
+    private static void change2(Data2 d) {
+        d.x = 1000;
+        System.out.println("change() : x = " + d.x);
+    }
+}
+
